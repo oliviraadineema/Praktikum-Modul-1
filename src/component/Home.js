175692f1c6742acc328cc63a33dbb01a60e5eac2
EZHAR SAVERO <olivira_adineema_29rpl@student.smktelkom-mlg.sch.id>
2022-03-11 15:01:45 +0700

    Reactjs

---
+++ src/component/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div className="dashboard ms-5">
+                    <p className='location-header'>PRAKTIKUM 1/Home</p>
+                    <br></br><br></br><br></br><br></br><br/><br/><br/>
+                            <h5 className="card-title display-3 fw-bolder mb-0">REACT</h5>
+                            <p className="card-text lead fs-2 mb-0">PRAKTIKUM 1</p>
+                            <p className="card-text lead mb-5">- OLIVIRA -</p>
+                         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+               
+            </div>
+                
+    );
+};
+
+export default Home;
